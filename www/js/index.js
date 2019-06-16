4414e0ad8443ee6dc83e1e0d223788fa6708559f
Ashwin <ashwin.alaparthi@gmail.com>
2019-06-16 08:50:28 +0530

    Added maps and countdown and stuff

--- www/js/index.js
+++ www/js/index.js
@@ -1,3 +1,8 @@
 $(document).ready(function () {
     $('.sidenav').sidenav();
+    $(".scroll_for_baral").click(function(){
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $(".content_title").offset().top
+        }, 1000);
+    });
 });
---
+++ www/js/maps.js
@@ -0,0 +1,47 @@
+var mymap = L.map('mapid').setView([12.934645, 77.535098], 17);
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiYXNoMTEwIiwiYSI6ImNqd3hpYzQyNTFmcmo0MGsxYmVodXNlMjEifQ.Q3t7t9MzKS5VsLcJw4Rr3A', {
+    // attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 30,
+    id: 'mapbox.satellite',
+    accessToken: 'pk.eyJ1IjoiYXNoMTEwIiwiYSI6ImNqd3hpYzQyNTFmcmo0MGsxYmVodXNlMjEifQ.Q3t7t9MzKS5VsLcJw4Rr3A'
+}).addTo(mymap);
+var MRD_icon = L.icon({
+    iconUrl: 'images/RedMarker.png',
+    iconSize: [22, 33], // size of the icon shadow
+    iconAnchor: [20, 25], // point of the icon which will correspond to marker's location
+    popupAnchor: [-10, -20] // point from which the popup should open relative to the iconAnchor
+});
+var MRD_Marker = L.marker([12.9354787, 77.5350228], { icon: MRD_icon }).addTo(mymap);
+MRD_Marker.bindPopup("<b>MRD Block</b>.");
+var B_icon = L.icon({
+    iconUrl: 'images/BlueMarker.png',
+    iconSize: [22, 33], // size of the icon shadow
+    iconAnchor: [20, 25], // point of the icon which will correspond to marker's location
+    popupAnchor: [-10, -20] // point from which the popup should open relative to the iconAnchor
+});
+var B_Marker = L.marker([12.935184, 77.53472], { icon: B_icon }).addTo(mymap);
+B_Marker.bindPopup("<b>B Block</b>.");
+var F_icon = L.icon({
+    iconUrl: 'images/GreenMarker.png',
+    iconSize: [22, 33], // size of the icon shadow
+    iconAnchor: [20, 25], // point of the icon which will correspond to marker's location
+    popupAnchor: [-10, -20] // point from which the popup should open relative to the iconAnchor
+});
+var F_Marker = L.marker([12.933514, 77.535589], { icon: F_icon }).addTo(mymap);
+F_Marker.bindPopup("<b>F Block</b>.");
+var G_icon = L.icon({
+    iconUrl: 'images/OrangeMarker.png',
+    iconSize: [22, 33], // size of the icon shadow
+    iconAnchor: [20, 25], // point of the icon which will correspond to marker's location
+    popupAnchor: [-10, -20] // point from which the popup should open relative to the iconAnchor
+});
+var G_Marker = L.marker([12.933668, 77.534090], { icon: G_icon }).addTo(mymap);
+G_Marker.bindPopup("<b>G Block</b>.");
+var PiR3_icon = L.icon({
+    iconUrl: 'images/PinkMarker.png',
+    iconSize: [22, 33], // size of the icon shadow
+    iconAnchor: [20, 25], // point of the icon which will correspond to marker's location
+    popupAnchor: [-10, -20] // point from which the popup should open relative to the iconAnchor
+});
+var PiR3 = L.marker([12.933720, 77.534568], { icon: PiR3_icon }).addTo(mymap);
+PiR3.bindPopup("<b>PiR3 Food Court</b>.");
